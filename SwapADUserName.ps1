1d36a3ba6c9d22e3617244539a536369cb54e139
tlbalazs <tothlaszlo.b@gmail.com>
2023-06-21 09:54:01 +0200

    Update SwapADUserName.ps1

--- SwapADUserName.ps1
+++ SwapADUserName.ps1
@@ -11,16 +11,16 @@ $Users = Get-ADUser -Filter * -SearchBase $UsersOU -Properties GivenName, Surnam
 	# Loop through all the users in a specified OU
 	foreach ($User in $Users)
 	{ 
-        # Swapping the GivenName and Surname properties during filling variables (For correcting FN and LN fields)
+        	# Swapping the GivenName and Surname properties during filling variables (For correcting FN and LN fields)
 		$firstName = $User.Surname
 		$lastName = $User.GivenName
 
-        # Swapping the GivenName and Surname properties for the current user
-        Set-ADUser -Identity $User -GivenName "$firstName"
-        Set-ADUser -Identity $User -Surname "$lastName"
+        	# Swapping the GivenName and Surname properties for the current user
+        	Set-ADUser -Identity $User -GivenName "$firstName"
+        	Set-ADUser -Identity $User -Surname "$lastName"
        
-        # Convert lastname to uppercase
-        $lastName = $lastName.ToUpper()	
+        	# Convert lastname to uppercase
+        	$lastName = $lastName.ToUpper()	
 		# The following line will adjust the DisplayName
 		Set-ADUser -Identity $User -DisplayName "$lastName, $firstName"
 		
